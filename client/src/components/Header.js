b48dff562308e0cf700ea379bf368ab06808551b
Rahul Kumar Singh <contactrahulkrsingh@gmail.com>
2024-01-29 00:53:47 +0530

    new changes

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className='p-5 bg-primary justify-between'>
+   <h1 className='text-secondary'>Rn csjncdjsdnc sd </h1>
+    </div>
+  )
+}
+
+export default header
